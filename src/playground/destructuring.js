f25086216380d0bbe01d05412a51090193247f94
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-09 18:55:26 -0700

    89. ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,32 +1,63 @@
-const person = {
-    name: 'Alejandro',
-    age: 24,
-    location: {
-        city: 'Ciudad Juarez',
-        temp: 15
-    }
-};
+// // object destructuring
 
-// destructure with default values
-const { name = 'Anonymous', age = 0 } = person;
+// const person = {
+//     name: 'Alejandro',
+//     age: 24,
+//     location: {
+//         city: 'Ciudad Juarez',
+//         temp: 15
+//     }
+// };
 
-// destructure with custom name
-const { city, temp: temperature } = person.location;
+// // destructure with default values
+// const { name = 'Anonymous', age = 0 } = person;
 
-console.log(`${name} is ${age}`);
+// // destructure with custom name
+// const { city, temp: temperature } = person.location;
 
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`);
-}
+// console.log(`${name} is ${age}`);
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`);
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher
+
+// console.log(publisherName);
+
+// ------
+
+// array destructuring
+
+const address = [
+    '1299 S Juniper Street',
+    'Philadelphia',
+    'Pennsylvania',
+    '19147'
+];
+
+// leave comma to indicate we skip address[0] element
+// we can also apply defaults
+const [, city = 'New York', state = 'New York'] = address;
+
+console.log(`You are in ${city}, ${state}`);
+
+// item name; small price, medium price, large price
+const item = [
+    'Coffee (hot)',
+    '$2.00',
+    '$2.50',
+    '$2.75'
+];
+
+const [itemName, , mediumSizePrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumSizePrice}`);
