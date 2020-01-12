13a1f0dfee1ffe9b77f06922af0591fa7fb5d362
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-11 22:40:02 -0700

    122. Testing User Interaction

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -22,3 +22,88 @@ test('Should render ExpenseForm with expense data', () => {
     expect(wrapper).toMatchSnapshot();
 
 });
+
+test('Should render error for invalid form submission', () => {
+
+    const wrapper = shallow(<ExpenseForm />);
+
+    // first snapshot: before the error
+    expect(wrapper).toMatchSnapshot();
+
+    // defining preventDefault, required since the onSubmit handler calls it
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+
+    // second snapshot: after the error
+    expect(wrapper).toMatchSnapshot();
+
+});
+
+test('Should set description on input change', () => {
+
+    const value = 'New description';
+
+    const wrapper = shallow(<ExpenseForm />);
+
+    // at(0) to get the first input (description): 0 is the index
+    // define target.value since the onChange function requires it to get the value
+    wrapper.find('input').at(0).simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(wrapper.state('description')).toBe(value);
+
+});
+
+test('Should set note on textarea change', () => {
+
+    const value = 'New note';
+
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find('textarea').simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(wrapper.state('note')).toBe(value);
+
+});
+
+test('Should set amount if valid input', () => {
+
+    const value = '22.50';
+
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find('input').at(1).simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(wrapper.state('amount')).toBe(value);
+
+});
+
+test('Should not set amount if invalid input', () => {
+
+    const value = '12.122';
+
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find('input').at(1).simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(wrapper.state('amount')).toBe('');
+
+});
