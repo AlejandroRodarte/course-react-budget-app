97f20b8d70bef4cc9d48aea9ea487a850f789568
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-12 19:29:58 -0700

    127. Testing ExpenseListFilters: Part II

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import moment from 'moment';
+
 import { shallow } from 'enzyme';
 
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
@@ -41,5 +43,75 @@ test('Should render ExpenseListFilters with alternate filters', () => {
     });
 
     expect(wrapper).toMatchSnapshot();
+
+});
+
+test('Should handle text change', () => {
+
+    const value = 'rent';
+
+    wrapper.find('input').at(0).simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(setTextFilter).toHaveBeenLastCalledWith(value);
+
+});
+
+test('Should sort by date', () => {
+
+    const value = 'date';
+
+    wrapper.setProps({
+        filters: altFilters
+    });
+
+    wrapper.find('select').simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(getSortByAction).toHaveBeenLastCalledWith(value);
+
+});
+
+test('Should sort by amount', () => {
+
+    const value = 'amount';
+
+    wrapper.find('select').simulate('change', {
+        target: {
+            value
+        }
+    });
+
+    expect(getSortByAction).toHaveBeenLastCalledWith(value);
+
+});
+
+test('Should handle date changes', () => {
+
+    const dates = {
+        startDate: moment(0).subtract(3, 'days'),
+        endDate: moment(0).add(4, 'days')
+    };
+
+    wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')(dates);
+
+    expect(setStartDate).toHaveBeenLastCalledWith(dates.startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(dates.endDate);
+
+});
+
+test('Should handle date focus change', () => {
+
+    const calendarFocused = 'startDate';
+
+    wrapper.find('withStyles(DateRangePicker)').prop('onFocusChange')(calendarFocused);
+
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 
 });
