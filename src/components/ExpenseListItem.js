8e5993db591e209e5b81422effbfc4b7c2f76ec3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-13 11:26:18 -0700

    138. New Feature Workflow

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import moment from 'moment';
+
+import numeral from 'numeral';
+
 import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
@@ -15,11 +19,9 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
         </Link>
 
         <p>
-            Amount: { amount }
-        </p>
-
-        <p>
-            Time of creation: { createdAt }
+            { numeral(amount / 100).format('$0,0.00') }
+            -
+            { moment(createdAt).format('MMMM Do, YYYY') }
         </p>
 
     </div>
