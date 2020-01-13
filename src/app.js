cc3f3a65d4e1fb2b800bce6de3bf331ad131c544
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-12 19:50:26 -0700

    131. Integrating Git into Our Project

--- src/app.js
+++ src/app.js
@@ -9,40 +9,12 @@ import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configure-store';
 
-import getVisibleExpenses from './selectors/expenses';
-
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    console.log(getVisibleExpenses(state.expenses, state.filters));
-});
-
-store.dispatch(addExpense({
-    description: 'Water bill',
-    amount: 4500
-}));
-
-store.dispatch(addExpense({
-    description: 'Gas bill',
-    createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 109500
-}));
-
-store.dispatch(setTextFilter('bill'));
-
-// setup redux store on our whole application
 const jsx = (
     <Provider store={ store }>
         <AppRouter />
