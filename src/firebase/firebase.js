48c2104bc9907e5b59a2f79ea40cc98db4ec9a06
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-13 21:24:07 -0700

    149. Array Data in Firebase: Part I

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,11 +13,83 @@ const firebaseConfig = {
     messagingSenderId: "49966050630",
     appId: "1:49966050630:web:5ebe6afc97e32565b68947",
     measurementId: "G-VYMFPQPZC8"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
+
+const expenses = [
+    {
+        description: 'First expense',
+        note: 'First expense note',
+        amount: 10000,
+        createdAt: -146000000
+    },
+    {
+        description: 'Second expense',
+        note: 'Second expense note',
+        amount: 85600,
+        createdAt: 298000000
+    },
+    {
+        description: 'Third expense',
+        note: 'Third expense note',
+        amount: 900,
+        createdAt: 5390000000
+    }
+];
+
+for (const expense of expenses) {
+    database
+        .ref('expenses')
+        .push(expense);
+}
+
+// database
+//     .ref('notes/-LyXGH9Mhd_u4iXkUWBE')
+//     .update({
+//         body: 'Buy food'
+//     });
+
+// database
+//     .ref('notes')
+//     .push({
+//         title: 'Course Topics',
+//         body: 'React Native, Angular, Python'
+//     });
+
+// const firebaseNotes = {
+//     notes: {
+//         ae4ff9ab: {
+//             id: '12',
+//             title: 'First note',
+//             body: 'This is my note'
+//         },
+//         d8ba37ed: {
+//             id: '761ase',
+//             title: 'Another note',
+//             body: 'This is another note'
+//         }
+//     }
+// };
+
+// const notes = [
+//     {
+//         id: '12',
+//         title: 'First note',
+//         body: 'This is my note'
+//     },
+//     {
+//         id: '761ase',
+//         title: 'Another note',
+//         body: 'This is another note'
+//     }
+// ];
+
+// database
+//     .ref('notes')
+//     .set(notes);
 
 // fetching data a single time
 // database
@@ -57,23 +129,23 @@ const database = firebase.database();
 //         .set(30);
 // }, 10500);
 
-const onValueChange = 
-    database
-        .ref()
-        .on('value', snapshot => {
-            const { name, job } = snapshot.val();
-            console.log(`${name} is a ${job.title} at ${job.company}.`);
-        }, e => console.log(e));
+// const onValueChange = 
+//     database
+//         .ref()
+//         .on('value', snapshot => {
+//             const { name, job } = snapshot.val();
+//             console.log(`${name} is a ${job.title} at ${job.company}.`);
+//         }, e => console.log(e));
 
-setTimeout(() => {
-    database
-        .ref()
-        .update({
-            name: 'Alejandro',
-            'job/title': 'Dev',
-            'job/company': 'Netflix'
-        })
-}, 3000);
+// setTimeout(() => {
+//     database
+//         .ref()
+//         .update({
+//             name: 'Alejandro',
+//             'job/title': 'Dev',
+//             'job/company': 'Netflix'
+//         })
+// }, 3000);
 
 // setTimeout(() => database.ref().off(onValueChange), 5000);
 
