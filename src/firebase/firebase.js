e0c2e23e96c3e4c3bc9d9437826afef4c3a51f8e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-13 21:04:56 -0700

    148. Fetching Data From Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -19,72 +19,130 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database
-    .ref()
-    .set({
-        name: 'Alejandro Rodarte',
-        age: 24,
-        stressLevel: 6,
-        job: {
-            title: 'Software Developer',
-            company: 'Google'
-        },
-        location: {
-            city: 'Juarez',
-            country: 'Mexico'
-        }
-    })
-    .then(() => console.log('Data is saved!'))
-    .catch(error => console.log('This failed', error));
+// fetching data a single time
+// database
+//   .ref()
+//   .once('value')
+//   .then(snapshot => {
+//       const val = snapshot.val();
+//       console.log(val);
+//   })
+//   .catch(e => console.log('Error fetching data.', e));
 
-database
-    .ref()
-    .update({
-        stressLevel: 9,
-        'job/company': 'Amazon',
-        'location/city': 'Seattle'
-    })
-    .then(() => console.log('Data is updated!'))
-    .catch(error => console.log('This failed', error));
+// subscribing to changes in data
+// const onValueChange = 
+//     database
+//         .ref()
+//         .on('value', snapshot => {
+//             const val = snapshot.val();
+//             console.log(val);
+//         }, e => console.log('Error fetching data.', e));
 
-database
-    .ref('attributes')
-    .set({
-        height: 175,
-        weight: 58
-    })
-    .then(() => console.log('Attributes are saved!'))
-    .catch(error => console.log('This failed', error));
+// setTimeout(() => {
+//     database
+//         .ref('age')
+//         .set(29);
+// }, 3500);
 
-database
-    .ref('isSingle')
-    .set(null)
-    .then(() => console.log('Single status deleted!'))
-    .catch(error => console.log('This failed', error));
+// unsubscriptions
+// setTimeout(() => {
+//     database
+//         .ref()
+//         .off(onValueChange);
+// }, 7000);
 
-database
-    .ref('isSingle')
-    .remove()
-    .then(() => console.log('Single status deleted!'))
-    .catch(error => console.log('This failed', error));
+// setTimeout(() => {
+//     database
+//         .ref('age')
+//         .set(30);
+// }, 10500);
 
-// with update() we can update properties, create new properties and delete properties
-database
-    .ref()
-    .update({
-        name: 'Magdaleno Mendoza',
-        age: 35,
-        job: 'Software developer',
-        isSingle: null
-    })
-    .then(() => console.log('Data was updated!'))
-    .catch(error => console.log('This failed', error));
+const onValueChange = 
+    database
+        .ref()
+        .on('value', snapshot => {
+            const { name, job } = snapshot.val();
+            console.log(`${name} is a ${job.title} at ${job.company}.`);
+        }, e => console.log(e));
 
-// database with ref() ONLY updates at the root level
-// how to work with nested updates
-database
-    .ref()
-    .update({
-        job: 'Manager',
-        'location/city': 'Boston'
-    });
+setTimeout(() => {
+    database
+        .ref()
+        .update({
+            name: 'Alejandro',
+            'job/title': 'Dev',
+            'job/company': 'Netflix'
+        })
+}, 3000);
+
+// setTimeout(() => database.ref().off(onValueChange), 5000);
+
+// database
+//     .ref()
+//     .set({
+//         name: 'Alejandro Rodarte',
+//         age: 24,
+//         stressLevel: 6,
+//         job: {
+//             title: 'Software Developer',
+//             company: 'Google'
+//         },
+//         location: {
+//             city: 'Juarez',
+//             country: 'Mexico'
+//         }
+//     })
+//     .then(() => console.log('Data is saved!'))
+//     .catch(error => console.log('This failed', error));
+
+// database
+//     .ref()
+//     .update({
+//         stressLevel: 9,
+//         'job/company': 'Amazon',
+//         'location/city': 'Seattle'
+//     })
+//     .then(() => console.log('Data is updated!'))
+//     .catch(error => console.log('This failed', error));
+
+// database
+//     .ref('attributes')
+//     .set({
+//         height: 175,
+//         weight: 58
+//     })
+//     .then(() => console.log('Attributes are saved!'))
+//     .catch(error => console.log('This failed', error));
+
+// database
+//     .ref('isSingle')
+//     .set(null)
+//     .then(() => console.log('Single status deleted!'))
+//     .catch(error => console.log('This failed', error));
+
+// database
+//     .ref('isSingle')
+//     .remove()
+//     .then(() => console.log('Single status deleted!'))
+//     .catch(error => console.log('This failed', error));
+
+// // with update() we can update properties, create new properties and delete properties
+// database
+//     .ref()
+//     .update({
+//         name: 'Magdaleno Mendoza',
+//         age: 35,
+//         job: 'Software developer',
+//         isSingle: null
+//     })
+//     .then(() => console.log('Data was updated!'))
+//     .catch(error => console.log('This failed', error));
+
+// // database with ref() ONLY updates at the root level
+// // how to work with nested updates
+// database
+//     .ref()
+//     .update({
+//         job: 'Manager',
+//         'location/city': 'Boston'
+//     });
